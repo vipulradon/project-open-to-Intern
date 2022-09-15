7f05e7235e5cad7f5962e590e17c0561ea23774d
Saquib <saquibparvez007@gmail.com>
2022-09-15 14:53:24 +0530

    new

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,9 +1,7 @@
 const internModel = require("../models/internModel");
-const emailValidator = require("email-validator");
 const collegeModel = require("../models/collegeModel");
-const { default: mongoose } = require("mongoose");
-const { find } = require("../models/internModel");
-const lowerCase = require("lower-case");
+const validator = require("validator");
+
 // ===============================================Regex For Validations================================================================================================
 function validateName($name) {
     var nameReg = /^[A-Za-z ]*$/;
@@ -48,7 +46,7 @@ const createIntern = async function (req, res) {
 
 
         if (!internData.email) return res.status(400).send({ status: false, msg: "Email is A Mandatory Field,Please Input Email" });
-        if (!emailValidator.validate(internData.email)) return res.status(400).send({ status: false, msg: "Invalid Email" });
+        if (!validator.isEmail(internData.email)) return res.status(400).send({ status: false, msg: "Invalid Email" });
 
         if (!internData.mobile) return res.status(400).send({ status: false, msg: "Mobile is A Mandatory Field,Please Input Mobile" });
         if (!validateMobile(internData.mobile)) return res.status(400).send({ status: false, msg: "InValid Mobile" });
@@ -65,12 +63,14 @@ const createIntern = async function (req, res) {
         let id = college._id;
 
 
-        let obj = {};
-        obj.name = internData.name;
-        obj.email = internData.email;
-        obj.mobile = internData.mobile;
-        obj.collegeId = id;
+        let obj = {
+            name : internData.name,
+            email : internData.email,
+            mobile : internData.mobile,
+            collegeId : id
 
+        }
+       
 
         let savedIntern = await internModel.create(obj);
         return res.status(201).send({ status: true, data: savedIntern });
