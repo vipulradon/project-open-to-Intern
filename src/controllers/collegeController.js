40ec277bccf96f7c131138e5ab970dc76a77a471
vipulradon <vipulc777@gmail.com>
2022-09-15 12:40:39 +0530

    readyToSubmit

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -20,7 +20,7 @@ function validateName($name) {
 
 const createCollege = async function (req, res) {
     try {
-        if (!req.body) return res.status(400).send({ status: false, msg: "Request Body Cant be Blank" })
+        if (Object.keys(req.body).length==0) return res.status(400).send({ status: false, msg: "Request Body Cant be Blank" })
         let collegeData = req.body;
 
         if (!collegeData.name) return res.status(400).send({ status: false, msg: "CollegeName is A Mandatory Field" });
@@ -54,22 +54,25 @@ const getCollegeDetails = async function (req, res) {
         let college = req.query.collegeName;
         if (!college) return res.status(400).send({ status: false, msg: "Please Provide Query Filter" });
         if (!validateName(college)) return res.status(400).send({ status: false, msg: "Invalid College Name" });
-        college.toUpperCase();
+        let college1 = college.toUpperCase();
 
         req.query.isDeleted = false;
-        let collegeDetails = await collegeModel.findOne({ name: college }).select({ name: 1, fullName: 1, logoLink: 1 });
+        let collegeDetails = await collegeModel.findOne({ name: college1 }).select({ name: 1, fullName: 1, logoLink: 1 });
         if (!collegeDetails) return res.status(404).send({ status: false, msg: "College Not Found" });
         let collegeid = collegeDetails._id;
 
 
         let internlist = await internModel.find({ collegeId: collegeid }).select({ name: 1, email: 1, mobile: 1 });
-if(internlist.length==0){
-    internlist = "No Interns Have Applied For this College!!"
-}
+        if (internlist.length == 0) {
+            internlist = "No Interns Have Applied For this College!!"
+        }
 
-        let result = collegeDetails._doc;
-        result.interns = internlist;
-        delete result._id;
+        let result = {
+            name:collegeDetails.name,
+            fullName:collegeDetails.fullName,
+            logoLink:collegeDetails.logoLink,
+            interns:internlist
+        }
         return res.status(200).send({ status: true, data: result });
     }
     catch (error) {
